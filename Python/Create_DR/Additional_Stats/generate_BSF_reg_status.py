766201c556a9e442ce7a38d778f5befb7d1d850e
u-matt <Matthew.Bandura@communities.gov.uk>
2025-12-05 11:48:18 +0000

    fixed BSF_reg_status
    BSF_reg_status now combines the two months correctly and exports without the index column

--- Python/Create_DR/Additional_Stats/generate_BSF_reg_status.py
+++ Python/Create_DR/Additional_Stats/generate_BSF_reg_status.py
@@ -10,7 +10,7 @@ import openpyxl
 def generate_BSF_reg_status(BSF_cut, last_month_BSF_cut):
     #file is too complex to load normally so must be read_only
     BSF_wb = openpyxl.load_workbook(BSF_cut, read_only= True, data_only= True)
-    BSF_tables_DR = BSF_wb['Essential Tables for DR V2']
+    BSF_tables_DR = BSF_wb['Essential Tables for DR']
 
     #iterates through the rows containing the registration status table
     BSF_reg_status_data = BSF_tables_DR.iter_rows(min_row = 20, max_row = 29,
@@ -20,9 +20,9 @@ def generate_BSF_reg_status(BSF_cut, last_month_BSF_cut):
     #turns the data into a list rather than a generator type object
     BSF_reg_status_data = list(BSF_reg_status_data)
 
-    #turns the data into a pandas data fram with rows starting at 1 columns 0
+    #turns the data into a pandas data frame with rows starting at 1 columns 0
     BSF_reg_status_df = pd.DataFrame(BSF_reg_status_data[1:], columns  = BSF_reg_status_data[0])
-
+    BSF_reg_status_df.rename(columns={BSF_reg_status_df.columns[-1]: 'Current Month'}, inplace=True)
     BSF_wb.close()
 
 
@@ -39,10 +39,11 @@ def generate_BSF_reg_status(BSF_cut, last_month_BSF_cut):
     #turns the data into a list rather than a generator type object
     last_month_BSF_reg_status_data = list(last_month_BSF_reg_status_data)
 
-    #turns the data into a pandas data fram with rows starting at 1 columns 0
+    #turns the data into a pandas data frame with rows starting at 1 columns 0
     last_month_BSF_reg_status_df = pd.DataFrame(last_month_BSF_reg_status_data[1:], columns  = last_month_BSF_reg_status_data[0])
-
+    last_month_BSF_reg_status_df.rename(columns={last_month_BSF_reg_status_df.columns[-1]: 'Last Month'}, inplace=True)
     last_month_BSF_wb.close()
 
-    BSF_reg_status_df_combined = BSF_reg_status_df.join(last_month_BSF_reg_status_df)
+    #combine this and last month's into one df
+    BSF_reg_status_df_combined = pd.merge(last_month_BSF_reg_status_df, BSF_reg_status_df)
     return BSF_reg_status_df_combined
