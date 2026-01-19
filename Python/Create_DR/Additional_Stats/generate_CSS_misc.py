bfaf8cc66e4ab59cfa33398843876987ca6367c0
u-matt <Matthew.Bandura@communities.gov.uk>
2026-01-19 16:22:03 +0000

    passed dictionary in correct order

--- Python/Create_DR/Additional_Stats/generate_CSS_misc.py
+++ Python/Create_DR/Additional_Stats/generate_CSS_misc.py
@@ -13,8 +13,8 @@ def generate_CSS_misc():
  
     #UPDATE MONTHLY FROM HOMES ENGLAND
     CSS_GFA_this_month = 944
-    CSS_GFA_last_month = 889
+    CSS_GFA_last_month = 899
 
 
-    CSS_misc= pd.DataFrame({ 'Current Month': [CSS_transfers_this_month, CSS_GFA_this_month], 'Last Month': [CSS_transfers_last_month, CSS_GFA_last_month]}, index = ['CSS Transfers', 'GFA'])
+    CSS_misc= pd.DataFrame({ 'Current Month': [CSS_GFA_this_month, CSS_transfers_this_month], 'Last Month': [CSS_GFA_last_month, CSS_transfers_last_month]}, index = ['CSS Transfers', 'GFA'])
     return CSS_misc
