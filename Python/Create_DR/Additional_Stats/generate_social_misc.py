7b3e6eb44c4f1edd9017e71233a1c7b943447af1
u-matt <Matthew.Bandura@communities.gov.uk>
2026-02-09 11:56:08 +0000

    created automated TS
    automated production of a time series showing how the unknown and eligibility pending figures have evolved over time

--- Python/Create_DR/Additional_Stats/generate_social_misc.py
+++ Python/Create_DR/Additional_Stats/generate_social_misc.py
@@ -1,22 +1,36 @@
 '''
 Author: Matthew Bandura
-Created on Thursday 20th November, 14:12:17  
+Created on Wednesday 4th January, 13:26:17  
 '''
 
 import pandas as pd
+import openpyxl
 
-def generate_social_misc():
-    #UPDATE QUARTERLY FROM FELICIA
-    social_respondant_rps = 549
-    social_excluded_rps = 939
+def generate_social_misc(MI_tables, month, year, last_month):
 
-    #UPDATE MONTHLY FROM FELICIA
-    social_in_other_programmes = 148
+    month_year = month[:3] + '-' + str(year)[2:] +'1'
 
-    #UPDATE MONTHLY FROM FILTERING PORTFOLIO LIST
-    social_not_in_other_programmes = 1165
+    #UPDATE AUTOMATICALLY
+    MI_wb = openpyxl.load_workbook(MI_tables)
+    Combined_2 = MI_wb['Combined_2']
 
-    social_misc= pd.DataFrame({ 'Number': [social_respondant_rps, social_excluded_rps, social_in_other_programmes, social_not_in_other_programmes]}, index = ['Respondant RPs', 'Excluded RPs', 'In other programmes', 'Not in other programmes'])
+    remediation_unknown = Combined_2['F17'].value
+    eligibility_pending_current = round(Combined_2['F18'].value, -2)
+
+    social_current = pd.DataFrame({f'{month_year}': [remediation_unknown, eligibility_pending_current], 'Number' : ['Remediation progress currently unknown', 'Social sector - Eligibility pending ']})
+
+    social_misc = pd.read_excel(f'Q:\\BSP\Automation\\DR Automation\\Excel_inputs\\[PUT ADDITIONAL DR STATS HERE]\\[AUTOMATION]\\Additional_stats_{month}.xlsx', sheet_name = 'Social_misc')
+
+    
+    social_misc = social_misc.set_index('Number')
+
+    social_current = social_current.set_index('Number')
+    social_misc = social_misc.join(social_current, how='outer')
+    social_misc = social_misc.reset_index()
+
+
+    MI_wb.close()
+
     return social_misc
 
     
